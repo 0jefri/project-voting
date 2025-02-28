824cf1d62177fbff91615e116572c07b3e7b6722
jefri agistar <jefri4876@gmail.com>
2025-03-01 01:16:33 +0700

    feat: create modal error edit mahasiswa

--- resources/views/admin/mahasiswa/edit.blade.php
+++ resources/views/admin/mahasiswa/edit.blade.php
@@ -14,25 +14,29 @@
         @method('PUT')
 
         <div class="mb-3">
-          <label class="form-label"><i class="fas fa-user"></i> Username:</label>
-          <input type="text" name="username" value="{{ $mahasiswa->username }}" required class="form-control">
-        </div>
-
-        <div class="mb-3">
           <label class="form-label"><i class="fas fa-id-card"></i> Nama:</label>
-          <input type="text" name="name" value="{{ $mahasiswa->name }}" required class="form-control">
+          <input type="text" name="name" value="{{ old('name', $mahasiswa->name) }}" required class="form-control">
+          @error('name')
+        <div class="text-danger">{{ $message }}</div>
+      @enderror
         </div>
 
         <div class="mb-3">
           <label class="form-label"><i class="fas fa-address-card"></i> NIM:</label>
-          <input type="text" name="nim" value="{{ $mahasiswa->detailMahasiswa->nim }}" required
+          <input type="text" name="nim" value="{{ old('nim', $mahasiswa->detailMahasiswa->nim) }}" required
           class="form-control">
+          @error('nim')
+        <div class="text-danger">{{ $message }}</div>
+      @enderror
         </div>
 
         <div class="mb-3">
           <label class="form-label"><i class="fas fa-book"></i> Program Studi:</label>
-          <input type="text" name="prodi" value="{{ $mahasiswa->detailMahasiswa->prodi }}" required
+          <input type="text" name="prodi" value="{{ old('prodi', $mahasiswa->detailMahasiswa->prodi) }}" required
           class="form-control">
+          @error('prodi')
+        <div class="text-danger">{{ $message }}</div>
+      @enderror
         </div>
 
         <div class="d-flex justify-content-between">
@@ -46,4 +50,37 @@
     </div>
     </div>
   </div>
+
+  <!-- Modal Error -->
+  <div class="modal fade @if($errors->any()) show @endif" id="errorModal" tabindex="-1" aria-labelledby="errorModalLabel"
+    aria-hidden="true" @if($errors->any()) style="display: block;" @endif>
+    <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header bg-danger text-white">
+      <h5 class="modal-title" id="errorModalLabel">Kesalahan Validasi</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <ul>
+        @foreach ($errors->all() as $error)
+      <li class="text-danger">{{ $error }}</li>
+    @endforeach
+      </ul>
+      </div>
+      <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+      </div>
+    </div>
+    </div>
+  </div>
+
+  <script>
+    document.addEventListener("DOMContentLoaded", function () {
+    // Jika ada error, otomatis tampilkan modal
+    if (@json($errors->any())) {
+      var errorModal = new bootstrap.Modal(document.getElementById("errorModal"));
+      errorModal.show();
+    }
+    });
+  </script>
 @endsection
